588ee0663f8ab138c60647fa80b1dd1e667cce05
libaicheng <1477996221@qq.com>
2024-12-27 18:24:24 +0800

    feat: 【node blog】

--- conf/db.js
+++ conf/db.js
@@ -1,59 +1,44 @@
-// const MYSQL_CONF = {
-//     host:'localhost',
-//     user:'root',
-//     password:'123456',
-//     port:'3306',
-//     database:'myblog'
-// };
-
-// const REDIS_CONF = {
-//     port:6379,
-//     host:'127.0.0.1'
-// }
-
-// module.exports = { MYSQL_CONF,REDIS_CONF };
-
-const env = process.env.NODE_ENV  // 环境参数
+const env = process.env.NODE_ENV // 环境参数
 
 // 配置
 let MYSQL_CONF
 let REDIS_CONF
 
 if (env === 'dev') {
-    // mysql
-    MYSQL_CONF = {
-        host:'localhost',
-        user:'root',
-        password:'592poker',
-        port:'3306',
-        database:'blog'
-    }
+  // mysql
+  MYSQL_CONF = {
+    host: '127.0.0.1',
+    user: 'root',
+    password: '123456',
+    port: '3306',
+    database: 'blog',
+  }
 
-    // redis
-    REDIS_CONF = {
-        port: 6379,
-        host: '127.0.0.1'
-    }
+  // redis
+  REDIS_CONF = {
+    port: 6379,
+    host: '127.0.0.1',
+  }
 }
 
 if (env === 'production') {
-    // mysql
-    MYSQL_CONF = {
-        host:'127.0.0.1',
-        user:'root',
-        password:'',
-        port:'3306',
-        database:''
-    }
+  // mysql
+  MYSQL_CONF = {
+    host: '127.0.0.1',
+    user: 'root',
+    password: '123456',
+    port: '3306',
+    database: 'blog',
+  }
 
-    // redis
-    REDIS_CONF = {
-        port: 6379,
-        host: '127.0.0.1'
-    }
+  // redis
+  REDIS_CONF = {
+    port: 6379,
+    host: '127.0.0.1',
+  }
 }
 
 module.exports = {
-    MYSQL_CONF,
-    REDIS_CONF
+  MYSQL_CONF,
+  REDIS_CONF,
 }
